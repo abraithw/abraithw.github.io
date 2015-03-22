0bcf196320e2f4acdd046b82e5041429d65483cf
tinaliang <tina.tang.tl@gmail.com>
2015-03-22 11:41:04 -0400

    js of emergency

---
+++ emergency.js
@@ -0,0 +1,93 @@
+function sex(e1){
+  var myElements = document.querySelectorAll(".sex")
+  for (var i=0; i<myElements.length; i++){
+    myElements[i].style.opacity = 1;
+  }
+  var bye=document.getElementById("hurt");
+  bye.parentNode.removeChild(bye);
+  var bye5=document.getElementById("ill");
+  bye5.parentNode.removeChild(bye5);
+  var bye1=document.getElementById("Counselor on Call");
+  bye1.parentNode.removeChild(bye1);
+  var bye2=document.getElementById("RA on Call");
+  bye2.parentNode.removeChild(bye2);
+  var bye3=document.getElementById("Dean on Call");
+  bye3.parentNode.removeChild(bye3);
+  var bye4=document.getElementById("ER");
+  bye4.parentNode.removeChild(bye4);
+
+
+function hurt(e1){
+var myElements = document.querySelectorAll(".hurt")
+  for (var i=0; i<myElements.length; i++){
+    myElements[i].style.opacity = 1;
+}
+  var bye=document.getElementById("sex");
+  bye.parentNode.removeChild(bye);
+  var bye5=document.getElementById("ill");
+  bye5.parentNode.removeChild(bye5);
+  var bye1=document.getElementById("Nearest Clergy");
+  bye1.parentNode.removeChild(bye1);
+  var bye2=document.getElementById("Nearest Pharmacy");
+  bye2.parentNode.removeChild(bye2);
+  var bye3=document.getElementById("Police");
+  bye3.parentNode.removeChild(bye3);
+  var bye4=document.getElementById("ER");
+  bye4.parentNode.removeChild(bye4);
+  var bye6=document.getElementById("Women's Center");
+  bye6.parentNode.removeChild(bye6);
+  var bye7=document.getElementById("EMS");
+  bye7.parentNode.removeChild(bye7);
+  var bye10=document.getElementById("Nearest Hospital");
+  bye10.parentNode.removeChild(bye10);
+
+function ill(e1){
+
+}
+
+  var bye=document.getElementById("sex");
+  bye.parentNode.removeChild(bye);
+  var bye5=document.getElementById("hurt");
+  bye5.parentNode.removeChild(bye5);
+  var bye1=document.getElementById("Nearest Clergy");
+  bye1.parentNode.removeChild(bye1);
+  var bye2=document.getElementById("Nearest Pharmacy");
+  bye2.parentNode.removeChild(bye2);
+  var bye3=document.getElementById("Police");
+  bye3.parentNode.removeChild(bye3);
+  var bye6=document.getElementById("Women's Center");
+  bye6.parentNode.removeChild(bye6);
+  var bye7=document.getElementById("EMS");
+  bye7.parentNode.removeChild(bye7);
+  var bye11=document.getElementById("Counselor on Call");
+  bye11.parentNode.removeChild(bye11);
+  var bye12=document.getElementById("RA on Call");
+  bye12.parentNode.removeChild(bye12);
+  var bye13=document.getElementById("Dean on Call");
+  bye13.parentNode.removeChild(bye13);
+
+
+function short(el) {
+  var myElements = document.querySelectorAll(".short")
+  for (var i=0; i<myElements.length; i++){
+    myElements[i].style.opacity = 1;
+  }
+  
+}
+
+
+function long(el) {
+  el.style.color="red";
+  el.style.width= "90%";
+  
+  var myElements = document.querySelectorAll(".tblong");
+  for (var i = 0; i < myElements.length; i++) {
+    myElements[i].style.opacity = 1;
+  }
+  var bye=document.getElementById("short");
+  bye.parentNode.removeChild(bye);
+  var bye1=document.getElementById("STINF");
+  bye1.parentNode.removeChild(bye1);
+  var bye2=document.getElementById("pharm");
+  bye2.parentNode.removeChild(bye2);
+}
